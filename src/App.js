d70b7e09bd250f6ad31512e10a3bd1025584af3c
blockodyssey-cheonga <cheonga@blockodyssey.io>
2022-06-04 02:08:07 +0900

    feat: Cart Modal 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React, { useState } from "react";
+
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartVisible, setCartVisible] = useState(false);
+
+  const showCartModalHandler = () => {
+    setCartVisible(true);
+  };
+
+  const hideCartModalHandle = () => {
+    setCartVisible(false);
+  };
+
   return (
     <>
-      <Header />
+      {cartVisible && <Cart onClose={hideCartModalHandle} />}
+      <Header onCart={showCartModalHandler} />
       <main>
         <Meals />
       </main>
---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[
+        {
+          id: "c1",
+          name: "Kimchi",
+          amount: 2,
+          price: 12.99,
+        },
+      ].map((item) => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {cartItems}
+      <div className={classes.total}>Total Amount</div>
+      <div>35.62</div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
